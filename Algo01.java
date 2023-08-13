0593c97bccce3d802980a5bdeb966a99dbb724e5
Tharindu Ishan <ishanthari96@gmail.com>
2023-08-13 17:06:13 +0530

    Algo1 done

--- Algo01.java
+++ Algo01.java
@@ -3,6 +3,28 @@ import java.util.Scanner;
 public class Algo01{
     private static final Scanner scanner=new Scanner(System.in);
     public static void main(String[] args) {
+        System.out.print("Input a number:");
+        int number = scanner.nextInt();
+        if(number<0) System.exit(2);
+
+
+
+        int f0=0;
+        int f1=1;
+        int n=2;
+        int ans;
+        if(number==0) {
+            System.out.println(0);
+            System.exit(2);}
+        do{
+
+            ans=f0+f1;
+            f0=f1;
+            f1=ans;
+            n++;
+
+        }while(number+1>n);
+        System.out.println(ans);
         
     }
 }
